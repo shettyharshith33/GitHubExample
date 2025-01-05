8bd6f5169a1ae19cb25d7ce1db9da464256ed0d8
shettyharshith33 <harshithshetty033@gmail.com>
2025-01-05 14:56:30 +0530

    Initial commit

---
+++ app/src/main/java/com/harshithshetty/githubexample/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.harshithshetty.githubexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.LinearGradient
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.harshithshetty.githubexample.ui.theme.GitHubExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+        }
+    }
+}
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun GitHubExample(modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(text = "Hello GitHub",
+            fontSize = 50.sp,
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.W900,
+            color = Color.DarkGray)
+    }
+}
